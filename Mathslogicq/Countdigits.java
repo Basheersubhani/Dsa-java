974d6558680dfde857636264099fd7d3aecf5594
Basheersubhani <gskbasheer4@gmail.com>
2026-09-25 21:12:52 +0530

    Added Countdigits

---
+++ Mathslogicq/Countdigits.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Countdigits
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while(n>0)
+        {
+            int lastdigit = n % 10;
+            count = count + 1;
+            n = n / 10;
+        }
+        System.out.println(count);
+    }
+}
